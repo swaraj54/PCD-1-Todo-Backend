8448248c89477fc7abcddac139643934b59f2a5c
Swaraj Jadhav <jadhavswaraj1920@gmail.com>
2024-06-17 13:03:43 +0530

    fix(code updated with env)

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from "express";
 import mongoose from "mongoose";
 import allRoutes from "./routes/index.js";
 import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("Working.");
@@ -14,9 +16,7 @@ app.get("/", (req, res) => {
 app.use("/api/v1", allRoutes);
 
 mongoose
-  .connect(
-    "mongodb+srv://myaccount:AO6GljLjvmKvVRzj@cluster0.6yd9l.mongodb.net/todo"
-  )
+  .connect(process.env.MONGODB_URL)
   .then(() => {
     console.log("Connected to MongoDB.");
   });
